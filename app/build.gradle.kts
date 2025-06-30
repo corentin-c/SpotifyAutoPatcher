75494b435479aafb3b8daafa6bb21ed59302acea
corentin-c <57543006+corentin-c@users.noreply.github.com>
2025-06-30 17:17:32 +0200

    Release 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.github.corentinc.SpotifyAutoPatcher"
         minSdk = 26
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.1.0"
         multiDexEnabled = true
     }
 
