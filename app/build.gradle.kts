6b33da62aa02797d9b95614c6b2eeabad3d2e4b0
corentin-c <57543006+corentin-c@users.noreply.github.com>
2025-07-03 15:27:09 +0200

    update dependencies and restore version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
 		minSdk = 26
 		targetSdk = 33
 		versionCode = 3
-		versionName = "1.0.0"
+		versionName = "1.1.0"
 		multiDexEnabled = true
 	}
 
@@ -58,13 +58,12 @@ dependencies {
 	coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 	implementation("com.google.android.material:material:1.12.0")
 	implementation("app.revanced:revanced-patcher:21.1.0-dev.1")
-	implementation("app.revanced:patches:5.29.0")
 	implementation("app.revanced:revanced-library-android:3.2.0-dev.1")
 	implementation("com.github.topjohnwu.libsu:nio:5.2.2")
 	implementation("com.google.code.gson:gson:2.13.1")
 
 	// remove if you don't want firebase
-	implementation(platform("com.google.firebase:firebase-bom:33.15.0"))
+	implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
 	implementation("com.google.firebase:firebase-crashlytics")
 	implementation("com.google.firebase:firebase-analytics")
 }
