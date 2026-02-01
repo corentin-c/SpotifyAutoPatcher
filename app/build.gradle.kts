1d86b13335c109b562db88eca1689a97ad5e9e86
Corentin C <57543006+corentin-c@users.noreply.github.com>
2026-02-01 21:19:56 +0100

    fix build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,92 +1,102 @@
 plugins {
-	id("com.android.application")
-	id("org.jetbrains.kotlin.plugin.compose") version "2.3.0"
-	// remove if you don't want firebase
-	id("com.google.gms.google-services")
-	id("com.google.firebase.crashlytics")
-	id("com.google.devtools.ksp")
-	id("com.google.dagger.hilt.android")
+    id("com.android.application")
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.0"
+    // remove if you don't want firebase
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
-	buildFeatures {
-		compose = true
-		buildConfig = true
-		viewBinding = false
-	}
+    buildFeatures {
+        compose = true
+        buildConfig = true
+        viewBinding = false
+    }
 
-	packaging {
-		jniLibs {
-			useLegacyPackaging = true
-		}
+    lint {
+        checkReleaseBuilds = false
+    }
+
+
+    packaging {
+        jniLibs {
+            useLegacyPackaging = true
+        }
         resources {
             excludes.add("prebuilt/**")
         }
-	}
+    }
 
-	namespace = "com.github.corentinc.SpotifyAutoPatcher"
-	compileSdk = 36
+    namespace = "com.github.corentinc.SpotifyAutoPatcher"
+    compileSdk = 36
 
-	defaultConfig {
-		applicationId = "com.github.corentinc.SpotifyAutoPatcher"
-		minSdk = 26
-		targetSdk = 33
-		versionCode = 5
-		versionName = "2.0.0"
-		multiDexEnabled = true
-	}
+    defaultConfig {
+        applicationId = "com.github.corentinc.SpotifyAutoPatcher"
+        minSdk = 26
+        targetSdk = 33
+        versionCode = 5
+        versionName = "2.0.0"
+        multiDexEnabled = true
+    }
 
-	buildTypes {
-		release {
-			isMinifyEnabled = true
-			isShrinkResources = true
-			proguardFiles(
-				getDefaultProguardFile("proguard-android-optimize.txt"),
-				"proguard-rules.pro"
-			)
-			signingConfig = signingConfigs.getByName("debug")
-		}
-	}
-	compileOptions {
-		isCoreLibraryDesugaringEnabled = true
-		sourceCompatibility = JavaVersion.VERSION_21
-		targetCompatibility = JavaVersion.VERSION_21
-	}
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
 
-	dependenciesInfo {
-		// Disables dependency metadata when building APKs.
-		includeInApk = false
-		// Disables dependency metadata when building Android App Bundles.
-		includeInBundle = false
-	}
+
+    dependenciesInfo {
+        // Disables dependency metadata when building APKs.
+        includeInApk = false
+        // Disables dependency metadata when building Android App Bundles.
+        includeInBundle = false
+    }
+}
+
+composeCompiler {
+    includeComposeMappingFile.set(false)
 }
 dependencies {
-	implementation("androidx.core:core-ktx:1.17.0")
-	coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
-	implementation("com.google.android.material:material:1.13.0")
-	implementation("app.revanced:revanced-patcher:21.1.0-dev.1")
-	implementation("app.revanced:revanced-library-android:3.2.0-dev.1")
-	implementation("com.github.topjohnwu.libsu:nio:6.0.0")
-	implementation("com.google.code.gson:gson:2.13.2")
+    implementation("androidx.core:core-ktx:1.17.0")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("app.revanced:revanced-patcher:21.1.0-dev.1")
+    implementation("app.revanced:revanced-library-android:3.2.0-dev.1")
+    implementation("com.github.topjohnwu.libsu:nio:6.0.0")
+    implementation("com.google.code.gson:gson:2.13.2")
 
-	// remove if you don't want firebase
-	implementation(platform("com.google.firebase:firebase-bom:34.8.0"))
-	implementation("com.google.firebase:firebase-crashlytics")
-	implementation("com.google.firebase:firebase-analytics")
+    // remove if you don't want firebase
+    implementation(platform("com.google.firebase:firebase-bom:34.8.0"))
+    implementation("com.google.firebase:firebase-crashlytics")
+    implementation("com.google.firebase:firebase-analytics")
 
 
-	// Core Compose libraries
-	implementation(platform("androidx.compose:compose-bom:2026.01.01"))
-	implementation("androidx.activity:activity-compose")
-	implementation("androidx.compose.ui:ui")
-	implementation("androidx.compose.ui:ui-tooling-preview")
-	implementation("androidx.compose.material3:material3")
-	implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
-	debugImplementation("androidx.compose.ui:ui-tooling")
-	debugImplementation("androidx.compose.ui:ui-test-manifest")
+    // Core Compose libraries
+    implementation(platform("androidx.compose:compose-bom:2026.01.01"))
+    implementation("androidx.activity:activity-compose")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
 
-	// Hilt
-	implementation("com.google.dagger:hilt-android:2.59")
-	ksp("com.google.dagger:hilt-compiler:2.59")
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.59")
+    ksp("com.google.dagger:hilt-compiler:2.59")
 }
