411595ebd2ce11d6d4a39255065265957cab4df3
corentin-c <57543006+corentin-c@users.noreply.github.com>
2026-04-20 16:57:01 +0200

    update everything

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.0"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.20"
     // remove if you don't want firebase
     id("com.google.gms.google-services")
     id("com.google.firebase.crashlytics")
@@ -142,7 +142,7 @@ androidComponents {
     }
 }
 dependencies {
-    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.core:core-ktx:1.18.0")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
     implementation("com.google.android.material:material:1.13.0")
     implementation("app.revanced:patcher:22.0.2-dev.1")
@@ -151,13 +151,13 @@ dependencies {
     implementation("com.google.code.gson:gson:2.13.2")
 
     // remove if you don't want firebase
-    implementation(platform("com.google.firebase:firebase-bom:34.8.0"))
+    implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
     implementation("com.google.firebase:firebase-crashlytics")
     implementation("com.google.firebase:firebase-analytics")
 
 
     // Core Compose libraries
-    implementation(platform("androidx.compose:compose-bom:2026.01.01"))
+    implementation(platform("androidx.compose:compose-bom:2026.03.01"))
     implementation("androidx.activity:activity-compose")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -168,6 +168,6 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
 
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.59")
-    ksp("com.google.dagger:hilt-compiler:2.59")
+    implementation("com.google.dagger:hilt-android:2.59.2")
+    ksp("com.google.dagger:hilt-compiler:2.59.2")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "9.0.0" apply false
+    id("com.android.application") version "9.1.1" apply false
     // remove if you don't want firebase
     id("com.google.gms.google-services") version "4.4.4" apply false
-    id("com.google.firebase.crashlytics") version "3.0.6" apply false
-    id("com.google.dagger.hilt.android") version "2.59" apply false
+    id("com.google.firebase.crashlytics") version "3.0.7" apply false
+    id("com.google.dagger.hilt.android") version "2.59.2" apply false
     id("com.google.devtools.ksp") version "2.3.5" apply false
 }
 
