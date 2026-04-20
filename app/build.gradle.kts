02d52dc2d9881c2882a7a0d7482fa954d3a417e8
corentin-c <57543006+corentin-c@users.noreply.github.com>
2026-04-20 16:58:31 +0200

    Update 2.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ android {
         applicationId = "com.github.corentinc.SpotifyAutoPatcher"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "2.2.0"
+        versionCode = 8
+        versionName = "2.3.0"
         multiDexEnabled = true
     }
 
